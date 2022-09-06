fd23d5d77103f0bd837f9664fb57e495d854616b
Rodrigo Ropelato <rodrigoropelato@hotmail.com>
2022-09-05 23:13:39 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NotificationButton from "./components/notificationButton"
 
 function App() {
-  return(
-    <h1>Ola Mundo!</h1>
+  return (
+    <>
+      <h1>Ola Mundo!</h1>
+      <NotificationButton />
+    </>
+
   )
 }
 
